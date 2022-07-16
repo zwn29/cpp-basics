1ee33acf5e8539319689aead495a81f2f47212ec
zenf <78023279+zencodee@users.noreply.github.com>
2022-07-17 05:17:18 +0530

    5th commit

---
+++ string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    // cout << "hello world";
+    // return 0;
+    // string greet = "Hello World!";
+    // cout << greet << endl;
+    string greet = "hii mom";
+    std::cout << greet << std::endl;
+    return 0;
+}
