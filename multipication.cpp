9d03788dc36c5ad12c5f7fd20083c3c08a9a7fe3
zenf <78023279+zencodee@users.noreply.github.com>
2022-07-17 05:00:36 +0530

    4th commit

--- multipication.cpp
+++ multipication.cpp
@@ -5,6 +5,14 @@ int main()
     int v1, v2;
     std::cout << "Enter two numbers:" << std::endl;
     std::cin >> v1 >> v2;
-    std::cout << "The multiplication of " << v1 << " and " << v2 << " is " << v1 * v2 << std::endl;
+    // std::cout << "The multiplication of " << v1 << " and " << v2 << " is " << v1 * v2 << std::endl;
+    // the below code is the same as the above commented code
+    std::cout << "The multiplication of ";
+    std::cout << v1;
+    std::cout << " and ";
+    std::cout << v2;
+    std::cout << " is ";
+    std::cout << v1 * v2;
+    std::cout << std::endl;
     return 0;
 }
