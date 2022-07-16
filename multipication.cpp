8f7041721a166585ffb0edcb47ed16a438d30e76
zenf <78023279+zencodee@users.noreply.github.com>
2022-07-17 04:53:10 +0530

    3rd commit

---
+++ multipication.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    int v1, v2;
+    std::cout << "Enter two numbers:" << std::endl;
+    std::cin >> v1 >> v2;
+    std::cout << "The multiplication of " << v1 << " and " << v2 << " is " << v1 * v2 << std::endl;
+    return 0;
+}
