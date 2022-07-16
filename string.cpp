70c27e8df990e232b7ab286dc7f22414df8b1f07
zenf <78023279+zencodee@users.noreply.github.com>
2022-07-17 05:20:17 +0530

    7th commit

--- string.cpp
+++ string.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
 int main()
 {
-    // cout << "hello world";
-    // return 0;
-    // string greet = "Hello World!";
-    // cout << greet << endl;
-    string greet = "hii mom";
-    std::cout << greet << std::endl;
+    // we can get rid of std by adding namespace std top of file
+    //  cout << "hello world";
+    //  return 0;
+    //  string greet = "Hello World!";
+    //  cout << greet << endl;
+    //  string greet = "hii mom";
+    //  std::cout << greet << std::endl;
+    string greet = "hii sia";
+    cout << greet << endl;
     return 0;
 }
