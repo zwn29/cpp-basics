eb3f3180f05bbf11d20a1057ff2433c969d4df1d
zenf <78023279+zencodee@users.noreply.github.com>
2022-07-17 13:13:35 +0530

    11th commit

---
+++ README.md
@@ -0,0 +1,1 @@
+![ Alt text](name-of-gif-file. gif) / ! [](name-of-gif-file. gif)
---
+++ first.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "welcome to the world of c++ and it is cool" << endl;
+    cin.get();
+    return 0;
+}
